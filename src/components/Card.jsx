f969fc379cade33c1896e17a03c1eb7e0dd0c497
SHADOW-TKLCCE3M\nazza <nazzareno.giannelli@gmail.com>
2024-12-27 14:28:26 +0100

    card and searchbox text matching colors

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,8 @@ const Card = ({ name, email, id }) => {
         <div className="p-4 text-center bg-green-200 rounded-3xl border-2 border-green-600 transition duration-300 transform hover:scale-105">
             <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
             <div className="pt-2">
-                <h2 className="text-2xl font-bold">{name}</h2>
-                <p className="font-bold">{email}</p>
+                <h2 className="text-2xl font-bold text-green-800">{name}</h2>
+                <p className="font-bold text-green-800">{email}</p>
             </div>
         </div>
     )
