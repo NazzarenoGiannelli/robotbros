e7db532bf12b084f455421c6ef7a284868d29f46
SHADOW-TKLCCE3M\nazza <nazzareno.giannelli@gmail.com>
2024-12-28 13:31:31 +0100

    transitioned from class to hooks in App.jsx. added a "click me" button counter

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,51 +1,46 @@
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import '../index.css'
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: '',
-        }
-    }
+export default function App() {
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+    const [count, setCount] = useState(0)
 
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then((response) => response.json())
-            .then((users) => this.setState({ robots: users }))
+            .then((users) => setRobots(users))
+    }, [])
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-
-    render() {
-        const { robots, searchfield } = this.state
-        const filteredRobots = robots.filter((robot) => {
-            return robot.name
-                .toLowerCase()
-                .includes(searchfield.toLowerCase())
-        })
-        return !robots.length ?
-        <h1>Loading</h1> :
-        (
-            <div className="min-h-screen bg-gradient-to-b from-cyan-800 to-green-400">
-                <div className="sticky top-0 z-10 justify-items-center pt-12 pb-4">
-                    <h1 className="mb-4 text-5xl text-center text-green-200 text-stroke-3 text-stroke-green-600 font-sega md:text-6xl">
-                        RobotBros
-                    </h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                </div>
-                <Scroll>
-                    <CardList robots={filteredRobots} />
-                </Scroll>
+    const filteredRobots = robots.filter((robot) => {
+        return robot.name
+            .toLowerCase()
+            .includes(searchfield.toLowerCase())
+    })
+    
+    return !robots.length ?
+    <h1>Loading</h1> :
+    (
+        <div className="min-h-screen bg-gradient-to-b from-cyan-800 to-green-400">
+            <div className="sticky top-0 z-10 justify-items-center pt-12 pb-4">
+                <h1 className="mb-4 text-5xl text-center text-green-200 text-stroke-3 text-stroke-green-600 font-sega md:text-6xl">
+                    RobotBros
+                </h1>
+                <button onClick={() =>setCount(count + 1)} className="px-4 py-2 text-xl font-bold text-green-800 bg-green-200 rounded-full border-2 border-green-600">
+                    Click Me<br/>{count}
+                </button>
+                <SearchBox searchChange={onSearchChange} />
             </div>
-        )
-    }
+            <Scroll>
+                <CardList robots={filteredRobots} />
+            </Scroll>
+        </div>
+    )
 }
-
-export default App
